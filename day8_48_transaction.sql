c092f14a4245d2fba0cc51a3ad9fbe4b30d10251
Masaya1582 <masa8y88@gmail.com>
2025-06-17 17:03:57 +0900

    day8まで終了

---
+++ day8_48_transaction.sql
@@ -0,0 +1,17 @@
+SHOW DATABASES;
+USE `day_4_9_db`;
+SHOW TABLES;
+
+UPDATE customers SET age = 44 WHERE id = 2;
+
+SELECT * FROM customers;
+UPDATE customers SET age = 44 WHERE id = 1;
+
+
+
+
+
+
+
+
+
---
+++ day8_51_transaction_lock.sql
@@ -0,0 +1,60 @@
+SHOW DATABASES;
+USE `day_4_9_db`;
+SHOW TABLES;
+
+START TRANSACTION;
+SHOW TABLES;
+
+SELECT * FROM customers;
+
+# 主キーでUPDATE (行ロック)
+UPDATE customers SET age = 43 WHERE id = 1;
+
+ROLLBACK;
+
+# TABLE全体のロック
+START TRANSACTION;
+UPDATE customers SET age = 42 WHERE name = "河野 文典";
+
+ROLLBACK;
+
+# DELETE 
+START TRANSACTION;
+DELETE FROM customers WHERE id = 1;
+COMMIT;
+
+# INSERT
+INSERT INTO 
+customers
+VALUES(1, "田中 一郎", 21, "1999-01-01");
+
+# SELECTロック、FOR SHARE(共有ロック)、FOR UPDATE(排他ロック)
+START TRANSACTION;
+SELECT * FROM customers WHERE id = 1 FOR SHARE;
+ROLLBACK;
+
+START TRANSACTION;
+SELECT * FROM customers WHERE id = 1 FOR UPDATE;
+ROLLBACK;
+
+# 明示的にTABLEをロック
+LOCK TABLE customers READ;
+SELECT * FROM customers;
+UPDATE customers SET age = 42 WHERE id = 1;
+UNLOCK TABLES;
+
+# LOCK TABLE WRITE
+LOCK TABLE customers WRITE;
+SELECT * FROM customers;
+UNLOCK TABLES;
+
+# DEAD LOCK
+START TRANSACTION;
+ROLLBACK;
+
+
+
+
+
+
+
